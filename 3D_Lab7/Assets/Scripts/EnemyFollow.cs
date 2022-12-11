b9198119e922c3b3063180e5c37c0f5ca4e33f13
Yurii-Polischukkk <113755012+Yurii-Polischukkk@users.noreply.github.com>
2022-12-11 12:45:13 +0200

    lab7

---
+++ 3D_Lab7/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform Player;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        enemy.SetDestination(Player.position);
+    }
+}
